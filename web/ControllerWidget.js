a870abad77b8bd31e2756d7c29f79f9df446610b
Psychopoulet <svidal1@free.fr>
2016-09-22 19:47:49 +0200

    lien de l'interface avec l'API

--- web/ControllerWidget.js
+++ web/ControllerWidget.js
@@ -1,190 +1,211 @@
-app.controller('ControllerWarcraftSounds', ['$scope', '$actions', '$popup', function($scope, $actions, $popup) {
+app.controller('ControllerWarcraftSounds', ['$scope', '$popup', "$MIA", function($scope, $popup, $MIA) {
 
 	"use strict";
 
-	// attributes
+	// public
 
-		// public
+		// attributes
 
-			$scope.races = $scope.characters = $scope.actions = $scope.musics = $scope.warnings = $scope.childs = [];
+			$scope.races = $scope.characters = $scope.actions = $scope.musics = $scope.warnings = $scope.devices = [];
 			$scope.selectedrace = $scope.selectedmusic = $scope.selectedwarning = $scope.selectedcharacter = null;
 
-	// methods
+		// interface
 
-		// selects
+			// selects
 
-			$scope.selectRace = function(race) {
+				$scope.selectRace = function(race) {
 
-				if (race) {
+					if (!race) {
+						$scope.characters = $scope.actions = $scope.musics = $scope.warnings = [];
+						$scope.selectedrace = $scope.selectedmusic = $scope.selectedwarning = $scope.selectedcharacter = null;
+					}
+					else {
 
-					$scope.selectedrace = race;
+						$scope.selectedrace = race;
 
-					socket.emit('plugin.warcraft3sounds.characters.get', { race : race });
-					socket.emit('plugin.warcraft3sounds.musics.get', { race : race });
-					socket.emit('plugin.warcraft3sounds.warnings.get', { race : race });
+						$MIA.url(race.charactersurl).then(function(data) {
 
-				}
-				else {
-					$scope.characters = $scope.actions = $scope.musics = $scope.warnings = [];
-					$scope.selectedrace = $scope.selectedmusic = $scope.selectedwarning = $scope.selectedcharacter = null;
-				}
+							$scope.characters = data;
+							$scope.selectCharacter((1 == data.length) ? data[0] : null);
+							
+						}).catch(function(err) {
 
-			};
+							$popup.alert({
+								title: "Chargement des personnages",
+								message: err.message,
+								type: "danger"
+							});
 
-			$scope.selectCharacter = function(race, character) {
+						});
 
-				if (race && character) {
-					$scope.selectedcharacter = character;
-					socket.emit('plugin.warcraft3sounds.actions.get', { race : race, character : character });
-				}
-				else {
-					$scope.selectedcharacter = null;
-					$scope.actions = [];
-				}
+						$MIA.url(race.musicsurl).then(function(data) {
 
-			};
+							$scope.musics = data;
+							$scope.selectedmusic = (1 == data.length) ? data[0] : null;
+				
+						}).catch(function(err) {
+
+							$popup.alert({
+								title: "Chargement des musiques",
+								message: err.message,
+								type: "danger"
+							});
+
+						});
+
+						$MIA.url(race.warningsurl).then(function(data) {
+
+							$scope.warnings = data;
+							$scope.selectedwarning = (1 == data.length) ? data[0] : null;
+				
+						}).catch(function(err) {
+
+							$popup.alert({
+								title: "Chargement des alertes",
+								message: err.message,
+								type: "danger"
+							});
+
+						});
+
+					}
+
+				};
+
+				$scope.selectCharacter = function(character) {
+
+					if (!character) {
+						$scope.selectedcharacter = null;
+						$scope.actions = [];
+					}
+					else {
+
+						$scope.selectedcharacter = character;
+
+						$MIA.url(character.actionsurl).then(function(data) {
+
+							$scope.actions = data;
+							$scope.selectedaction = (1 == data.length) ? data[0] : null;
+				
+						}).catch(function(err) {
+
+							$popup.alert({
+								title: "Chargement des actions",
+								message: err.message,
+								type: "danger"
+							});
+
+						});
+
+					}
+
+				};
 
 		// previews
 
 			$scope.previewMusic = function(race, music) {
 
-                $popup.sound({
-                	sources: [ music.url ],
-                	title: race.name + ' - ' + music.name
-                });
+				$popup.sound({
+					sources: [ music.soundurl ],
+					title: race.name + ' - ' + music.name
+				});
 
 			};
 			$scope.previewWarning = function(race, warning) {
 
-                $popup.sound({
-                	sources: [ warning.url ],
-                	title: race.name + ' - ' + warning.name
-                });
+				$popup.sound({
+					sources: [ warning.soundurl ],
+					title: race.name + ' - ' + warning.name
+				});
 
 			};
-            $scope.previewAction = function(race, character, action) {
-            	
-                $popup.sound({
-                	sources: [ action.url ],
-                	title: race.name + '/' + character.name + ' - ' + action.name
-                });
+			$scope.previewAction = function(race, character, action) {
+				
+				$popup.sound({
+					sources: [ action.soundurl ],
+					title: race.name + '/' + character.name + ' - ' + action.name
+				});
 
-            };
+			};
 
 		// plays
 
-			$scope.playMusicOnChild = function(child, music) {
+			$scope.playMusicOnChild = function(device, music) {
 
 				socket.emit('plugin.warcraft3sounds.music.play', {
-					child : child, music : music
+					device : device, music : music
 				});
 
 			};
-			$scope.playWarningOnChild = function(child, warning) {
+			$scope.playWarningOnChild = function(device, warning) {
 
 				socket.emit('plugin.warcraft3sounds.warning.play', {
-                    child : child, warning : warning
+					device : device, warning : warning
 				});
 
 			};
-            $scope.playActionOnChild = function(child, action) {
+			$scope.playActionOnChild = function(device, action) {
 
-                socket.emit('plugin.warcraft3sounds.action.play', {
-                    child : child, action : action
-                });
+				socket.emit('plugin.warcraft3sounds.action.play', {
+					device : device, action : action
+				});
 
-            };
+			};
 
 		// actions
 
-			$scope.createSound = function (race, child, sound) {
-				$actions.add(race.name + ' - ' + sound.name, child, $actions.getActionTypeByCommand('media.sound.play'), sound);
+			$scope.createSound = function (race, device, sound) {
+				$actions.add(race.name + ' - ' + sound.name, device, $actions.getActionTypeByCommand('media.sound.play'), sound);
 			};
 
-	// constructor
+	// socket
 
-		// events
+		socket.on('devices', function (devices) {
+			
+			$scope.$apply(function() {
 
-			// childs
+				$scope.devices = [];
+				angular.forEach(devices, function(device) {
 
-			socket.on('childs', function (childs) {
+					if (device.connected && 'ACCEPTED' == device.status.code) {
+						$scope.devices.push(device);
+					}
+
+				});
 				
-				$scope.$apply(function() {
+				$scope.selecteddevice = (1 == $scope.devices.length) ? $scope.devices[0] : null;
+				
+			});
 
-					$scope.childs = [];
-					angular.forEach(childs, function(child) {
+		}).on('plugin.warcraft3sounds.error', function(err) {
 
-						if (child.connected && 'ACCEPTED' == child.status.code) {
-							$scope.childs.push(child);
-						}
+			$popup.alert({
+				title: "Warcraft 3 sounds",
+				message: err,
+				type: 'danger'
+			});
 
-					});
-					
-					$scope.selectedchild = (1 == $scope.childs.length) ? $scope.childs[0] : null;
-					
-				});
-
-		    });
+		});
 	
-			// sockets
+	// init
+	
+		$MIA.onLogin(function() {
 
-				socket.on('plugin.warcraft3sounds.races.get', function (races) {
+			$MIA.url("/api/plugins/warcraft3sounds/races").then(function(data) {
 
-					$scope.$apply(function() {
+				$scope.races = data;
+				$scope.selectRace((1 == data.length) ? data[0] : null);
+				
+			}).catch(function(err) {
 
-						$scope.races = races;
-						$scope.selectRace((1 == races.length) ? races[0] : null);
-						
-					});
-
-				})
-				.on('plugin.warcraft3sounds.characters.get', function (characters) {
-
-					$scope.$apply(function() {
-
-						$scope.characters = characters;
-						$scope.selectCharacter($scope.selectedrace, (1 == characters.length) ? characters[0] : null);
-						
-					});
-
-				})
-					.on('plugin.warcraft3sounds.actions.get', function (actions) {
-
-						$scope.$apply(function() {
-
-							$scope.actions = actions;
-							$scope.selectedaction = (1 == actions.length) ? actions[0] : null;
-							
-						});
-
-					})
-				.on('plugin.warcraft3sounds.musics.get', function (musics) {
-
-					$scope.$apply(function() {
-
-						$scope.musics = musics;
-						$scope.selectedmusic = (1 == musics.length) ? musics[0] : null;
-						
-					});
-
-				})
-				.on('plugin.warcraft3sounds.warnings.get', function (warnings) {
-					
-					$scope.$apply(function() {
-
-						$scope.warnings = warnings;
-						$scope.selectedmusic = (1 == warnings.length) ? warnings[0] : null;
-						
-					});
-
-				})
-				.on('plugin.warcraft3sounds.error', function(err) {
-
-					$popup.alert({
-						message: err,
-						type: 'danger'
-					});
-
+				$popup.alert({
+					title: "Chargement des races",
+					message: err.message,
+					type: "danger"
 				});
+
+			});
+
+		});
 
 }]);
